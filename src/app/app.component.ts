8a7f6e87ef59d5bd8cf571eafba85e46a7453810
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2022-05-08 20:28:14 +0530

    tic-tac-toe web app created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,83 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic-tac-toe';
+  winnerMessage: string = "";
+  isCross: boolean = false;
+  itemArr = new Array(9).fill('empty');
+  gameOverFlag: boolean = false;
+
+  constructor(private toastr: ToastrService){}
+
+  resetGame = () => {
+    this.winnerMessage = "";
+    this.isCross = false;
+    this.itemArr = new Array(9).fill('empty');
+  }
+
+  handleClick = (num: number) => {
+    if(!this.winnerMessage && this.itemArr[num] != 'empty') {
+      this.toastr.error("Nobody won!");
+      this.gameOverFlag = true;
+    }
+    else if(this.winnerMessage) {
+      this.toastr.success(this.winnerMessage);
+    }
+    else if(this.itemArr[num] != 'empty') {
+      this.toastr.info("Already filled!");
+    }
+    else {
+      this.itemArr[num] = this.isCross ? 'cross' : 'circle';
+      this.isCross = !this.isCross;
+    }
+    this.isWinner();
+  }
+
+  isWinner = () => {
+    if(this.itemArr[0] != 'empty' && this.itemArr[0] == this.itemArr[1] && this.itemArr[1] == this.itemArr[2]) {
+      this.winnerMessage = `${this.itemArr[0]} won`;
+    }
+    else if (
+      this.itemArr[3] !== 'empty' &&
+      this.itemArr[3] === this.itemArr[4] &&
+      this.itemArr[4] === this.itemArr[5]
+    ) {
+      this.winnerMessage = `${this.itemArr[3]} won`;
+    } else if (
+      this.itemArr[6] !== 'empty' &&
+      this.itemArr[6] === this.itemArr[7] &&
+      this.itemArr[7] === this.itemArr[8]
+    ) {
+      this.winnerMessage = `${this.itemArr[6]} won`;
+    } else if (
+      this.itemArr[0] !== 'empty' &&
+      this.itemArr[0] === this.itemArr[3] &&
+      this.itemArr[3] === this.itemArr[6]
+    ) {
+      this.winnerMessage = `${this.itemArr[0]} won`;
+    } else if (
+      this.itemArr[1] !== 'empty' &&
+      this.itemArr[1] === this.itemArr[4] &&
+      this.itemArr[4] === this.itemArr[7]
+    ) {
+      this.winnerMessage = `${this.itemArr[1]} won`;
+    } else if (
+      this.itemArr[2] !== 'empty' &&
+      this.itemArr[2] === this.itemArr[5] &&
+      this.itemArr[5] === this.itemArr[8]
+    ) {
+      this.winnerMessage = `${this.itemArr[2]} won`;
+    } else if (
+      this.itemArr[0] !== 'empty' &&
+      this.itemArr[0] === this.itemArr[4] &&
+      this.itemArr[4] === this.itemArr[8]
+    ) {
+      this.winnerMessage = `${this.itemArr[0]} won`;
+    } else if (
+      this.itemArr[2] !== 'empty' &&
+      this.itemArr[2] === this.itemArr[4] &&
+      this.itemArr[4] === this.itemArr[6]
+    ) {
+      this.winnerMessage = `${this.itemArr[2]} won`;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { IconComponent } from './icon/icon.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
